639a63935a045278d5b5552624dde87224732e2d
Kai1901 <truonggiakhanham@gmail.com>
2026-02-01 23:20:36 +0100

    Exercise 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { Routes, Route } from 'react-router'
 import { OrdersPage } from './pages/OrdersPage'
 import { Tracking } from './pages/Tracking'
