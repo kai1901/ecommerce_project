917c490175fc27877b488d5069e2555f8536f2ef
Kai1901 <truonggiakhanham@gmail.com>
2026-02-03 18:27:08 +0100

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,16 @@ import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        // fetch() sẽ cần một khoảng thời gian để tải hết dữ liệu từ backend
+        // trong khi đó đoạn code sẽ chạy hết các dòng code trong return
+        // sau khi đã load xong data sẽ chạy đoạn code trong then()
+        .then((response) => {
+            response.json().then((data) => {
+                console.log(data); 
+            });
+        })
+
     return (
         <>
             <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
